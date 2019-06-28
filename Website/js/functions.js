604b6c68a586e74ef728d1a2af624ee29b033edd
Benedict Wong <benatparapet@hotmail.com>
2019-06-27 23:28:51 -0700

    initial design of portfolio homepage

---
+++ Website/js/functions.js
@@ -0,0 +1,14 @@
+function onHover(link){
+  if(link == 'about'){
+    document.getElementById("home-page-img").src = "img/placeholder-home.png";
+  }
+  if(link == 'projects'){
+    document.getElementById("home-page-img").src = "img/placeholder-home-1.png";
+  }
+  if(link == 'contact'){
+    document.getElementById("home-page-img").src = "img/placeholder-home.png";
+  }
+  if(link == 'style-guide'){
+    document.getElementById("home-page-img").src = "img/placeholder-home-1.png";
+  }
+}
